72749af661f3bbea4d9c02d19f171940014a9332
Nathan Woodbury <nathanswoodbury@gmail.com>
2016-09-13 17:53:41 -0600

    Config found absolutely not relatively

--- main.py
+++ main.py
@@ -31,14 +31,21 @@ If you cannot import `configparser`, make sure that you are running Python 3.
 """
 from sample_agent import SampleAgent
 import configparser
+import os
 
 URL = 'http://45.56.15.67:8680'  # TODO switch to production when ready
 
 
 if __name__ == '__main__':
     # Read authentication information
+
+    # Need to fetch an absolute path of the config file relative to the current
+    # file so that an execution of this script from another location (such as
+    # by a crontab) will not lose the loction of the config.
+    config_loc = os.path.join(os.path.dirname(__file__), 'config.ini')
+
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read(config_loc)
 
     agent = SampleAgent(config, URL)
     agent.execute()
