86a5258594a28576f4a32c43126298ee671bdc26
Mike <mikebbrodie@gmail.com>
2016-10-14 02:48:34 -0600

    adding lstm files

--- sample_agent/Agent_lstm.py
+++ sample_agent/Agent_lstm.py
@@ -1,11 +1,15 @@
 from base_agent import BaseAgent
+from lstm import *
 import numpy as np
 import pandas as pd
 import sys
 import os
+import os.path
 from scipy.optimize import linprog
 import cPickle as pickle
 import time
+import h5py
+from keras.models import load_model
 
 class LstmAgent(BaseAgent):
     """A sample agent.
@@ -24,30 +28,152 @@ class LstmAgent(BaseAgent):
 
     def __init__(self, config, url):
         BaseAgent.__init__(self, config, url)
+	
    
     def pull_data(self):
         cwd = os.getcwd()
+        #prices = self.prices('MSFT', type='n', n=10000) 
+        #print(prices.shape)
         tickers = self.ticker_list()
+        df = None
+        '''
+        with h5py.File('sample_agent/data.h5', 'w') as hf:
+            for t in tickers:
+                print t
+                try:
+                    #prices = self.prices(ticker=t.strip(),type='this-month')
+                    prices = self.prices(ticker=t,type='n',n=40000)
+                    
+                    #hf.create_dataset(t, data=prices)
+                    prices.to_hdf(cwd+'/sample_agent/data2/'+str(t)+'.h5')
+                    #prices.to_pickle(cwd+'/sample_agent/data/'+str(t)+'.pkl')
+                except:
+                    print(sys.exc_info()[0])
+                    print('error for stock: '+t)
+        '''
+        df_list = {}
+        df_list = []
+        count = 0
+        data = pickle.load(open('stock_data_dic','rb'))
         for t in tickers:
             print t
             try:
                 #prices = self.prices(ticker=t.strip(),type='this-month')
-                prices = self.prices(ticker=t.strip(),n=10000)
-                prices.to_pickle(cwd+'/sample_agent/data/'+str(t)+'.pkl')
+                #prices = self.prices(ticker=t,type='n',n=40000) 
+                prices = []
+                if t in data:
+                    prices = data[t]
+                else:
+                    continue
+
+                bid_name = 'bid_'+str(t)
+                ask_name = 'ask_'+str(t)
+                #prices.rename(index=str, columns={'bid': bid_name, "ask": ask_name})
+                prices.columns = [bid_name,ask_name]
+                #del prices['last']
+                #prices.drop_duplicates(inplace=True)
+                prices = prices[~prices.index.duplicated(keep='first')]
+                print(prices.shape)
+                df_list.append(prices)
+                #df_list[t] = prices
+                count = count + 1
+                #if count > 5:
+                #    break
+                #hf.create_dataset(t, data=prices)
+                #prices.to_hdf(cwd+'/sample_agent/data2/'+str(t)+'.h5')
+                #prices.to_pickle(cwd+'/sample_agent/data/'+str(t)+'.pkl')
             except:
                 print(sys.exc_info()[0])
                 print('error for stock: '+t)
-
+        #if count > 5:
+        #pickle.dump(df_list, open('stock_data_dic','wb'))
+        #sys.exit()
+        result = pd.concat(df_list, axis=1, join='inner')
+        pickle.dump(result, open('stock_concat.pkl','wb'))
+        sys.exit()
+        #if still memory error, try jion=df1.index
+        import gc
+        df = df_list[0]
+        for i,df2 in enumerate(df_list[1:]):
+            df = df.join(df2, rsuffix='_'+tickers[i+1])
+            gc.collect()
+            #df = pd.merge(df, df2, left_index=True, 
+        #df = df.join(df_list[1:])
+        df.to_pickle(cwd+'/sample_agent/stock_data_join.pkl')
+        sys.exit()
+    def create_dataset_for_stock(self, data, stock):
+        X = []
+        y = []
+        min_look_back = 15
+        for i in range(data.shape[0] - 60 - 1):
+            X.append(data[i:i+min_look_back-1].values)
+            y.append(data['bid'+'_'+stock][i+60]) 
+            
+        return np.asarray(X), np.asarray(y)
     def create_initial_lstm_models(self):
         '''
         pull down all data and save
         what data will we pull? historic? bid? ask? or all        
         '''
+        data = pickle.load(open('stock_concat.pkl','rb'))        
+        tickers = self.ticker_list()
+        for t in tickers:
+            if 'bid_'+str(t) in data:
+                l = LSTM_RNN(t)
+                trainX, trainY = self.create_dataset_for_stock(data, t)
+                l.train(trainX,trainY) 
+                l.save('sample_agent/lstm_models/'+str(t)+'.h5') 
+                #break
+                   
+        
+    def update_lstm_models(self):
+        '''
+        use last 2 hours worth of data
+        to update all existing lstm models
+        '''
+        data = pickle.load(open('stock_concat_tmp.pkl'),'rb')
+        tickers = self.ticker_list()
+        for t in tickers:
+            if 'bid_'+str(t) in data and os.path.isfile('sample_agent/lstm_models/'+str(t)+'.h5'):            
+                
 
+    def pull_last_2_hours_data(self):
+        '''
+        pull down last 120 points of data
+        store as stock_concat_tmp.pkl
+        use to update lstm model weights
+        '''
+        tickers = self.ticker_list()
+        df_list = []
+        count = 0
+        for t in tickers:
+            print t
+            try:
+                data = self.prices(ticker=t,type='n',n=120) 
+                prices = []
+                if t in data:
+                    prices = data[t]
+                else:
+                    continue
+                del prices['last']
+                bid_name = 'bid_'+str(t)
+                ask_name = 'ask_'+str(t)
+                prices.columns = [bid_name,ask_name]
+                prices = prices[~prices.index.duplicated(keep='first')]
+                df_list.append(prices)
+                count = count + 1
+            except:
+                print(sys.exc_info()[0])
+                print('error for stock: '+t)
+        result = pd.concat(df_list, axis=1, join='inner')
+        pickle.dump(result, open('stock_concat_tmp.pkl','wb'))
+ 
+         
     def execute(self):
         """The main agent logic. Computes and executes a single trade.
         """
-        self.pull_data()
+        #self.pull_data()
+        self.create_initial_lstm_models()
         sys.exit()
         composition = {}
         for i in range(0,len(portfolio)):
---
+++ sample_agent/lstm.py
@@ -0,0 +1,99 @@
+import numpy
+import matplotlib.pyplot as plt
+import pandas
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+import cPickle as pickle
+
+def create_model():
+    '''creates a simple LSTM model for stock regression'''
+    model = Sequential()
+    #model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True))
+    #(6507, 1002)
+    model.add(LSTM(input_shape=(14,1002),output_dim=1000, activation='sigmoid'))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model 
+
+class LSTM_RNN:
+    def __init__(self, stock):
+        self.model = create_model() 
+        self.stock = stock
+
+    def train(self, trainX, trainY):
+        '''train on 1-month of data to initialize model parameters'''
+        #data = pickle.load(open(self.stock+'.p','rb'))
+        batch_size = 20
+        self.model.fit(trainX, trainY, nb_epoch=10, batch_size=batch_size, verbose=2, validation_split=0.15, shuffle=False)
+        self.model.reset_states()
+
+    def predict(self, X):
+        '''train on most recent 15 minutes to fine-tune weights'''
+        #self.model.
+
+    def save(self, name):
+        self.model.save(name)
+
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-look_back-1):
+        a = dataset[i:(i+look_back), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + look_back, 0])
+    return numpy.array(dataX), numpy.array(dataY)
+# fix random seed for reproducibility
+
+'''
+numpy.random.seed(7)
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+# reshape into X=t and Y=t+1
+look_back = 3
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+# reshape input to be [samples, time steps, features]
+trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+# create and fit the LSTM network
+batch_size = 1
+model = Sequential()
+for i in range(100):
+    model.fit(trainX, trainY, nb_epoch=1, batch_size=batch_size, verbose=2, shuffle=False)
+    model.reset_states()
+# Estimate model performance
+trainScore = model.evaluate(trainX, trainY, batch_size=batch_size, verbose=0)
+model.reset_states()
+trainScore = math.sqrt(trainScore)
+trainScore = scaler.inverse_transform(numpy.array([[trainScore]]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = model.evaluate(testX, testY, batch_size=batch_size, verbose=0)
+model.reset_states()
+testScore = math.sqrt(testScore)
+testScore = scaler.inverse_transform(numpy.array([[testScore]]))
+print('Test Score: %.2f RMSE' % (testScore))
+# generate predictions for training
+trainPredict = model.predict(trainX, batch_size=batch_size)
+testPredict = model.predict(testX, batch_size=batch_size)
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+# plot baseline and predictions
+plt.plot(dataset)
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
+'''      
